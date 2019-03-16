8b0b1c72ac7a1d065fc5df5677e414c4062a352b
Zerorlis <zerorlis@163.com>
2019-03-16 13:51:24 +0800

    修改了在其他路径没法读取到图片的bug

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Tomato-bell
+一个番茄钟的shell脚本，当番茄事件结束的时候，出现一张图片和铃声
+使用方法:./tomato.sh [time] &
+默认的时间是25分钟，可以提供其他的时间
+建议在bashrc里面建立这个命令的缩写alias
+alias to='function __tomato() { ~/shellproject/tomatoBell/tomato.sh $1 &  unset -f __tomato; }; __tomato'
--- tomato.sh
+++ tomato.sh
@@ -1,4 +1,5 @@
 #!/bin/sh    
+echo $0
 sleep_min=25
 if [ -n "$1" ];then
     sleep_min=$1
@@ -7,5 +8,8 @@ sleep_second=`expr $sleep_min \* 60`
 echo "番茄钟启动,时间是$sleep_min分钟"
 sleep $sleep_second
 echo "番茄钟完成"
-eog -f time.jpg & 
-play time.mp3 1>/dev/null 2>&1 &
+# 要试着获取当前脚本的路径，因为执行这个脚本不一定在这个同级目录下面
+dir=`dirname $0`
+echo $dir
+eog -f "$dir/time.jpg" & 
+play "$dir/time.mp3" 1>/dev/null 2>&1 &
