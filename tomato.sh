1f569b437a295604a17a6ae61e4b45eee599308f
VoidYin <iyintao@gmail.com>
2022-04-26 17:00:33 +0800

    Update tomato.sh

--- tomato.sh
+++ tomato.sh
@@ -1,13 +1,29 @@
-#!/bin/sh    
-sleep_min=25
-if [ -n "$1" ];then
-    sleep_min=$1
-fi
-sleep_second=`expr $sleep_min \* 60`
-echo "番茄钟启动,时间是$sleep_min分钟\n"
-sleep $sleep_second
-echo "番茄钟完成\n"
-# 要试着获取当前脚本的路径，因为执行这个脚本不一定在这个同级目录下面
+#!/bin/bash    
+
+set -ue
+
+sleeptime1=$((25*60))
+sleeptime2=$((5*60))
+
+#sleeptime1=$((10))
+#sleeptime2=$((5))
+
 dir=`dirname $0`
-eog -f "$dir/time.jpg" & 
-play "$dir/time.mp3" 1>/dev/null 2>&1 &
+
+timer1(){
+        sleep $sleeptime1
+        feh -F "${dir}/time.jpg" >/dev/null 2>&1 &
+}
+
+timer2(){
+        sleep $sleeptime2
+        pkill -f feh
+}
+
+echo "番茄钟启动 `date +"%Y.%m.%d %T"`"
+
+while true ; do
+        timer1
+        timer2
+done
+
